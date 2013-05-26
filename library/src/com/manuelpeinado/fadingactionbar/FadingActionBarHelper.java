507b09a39881a0368d522768e0277bbd131e6f6d
Manuel Peinado <manuel.peinado@gmail.com>
2013-05-26 17:51:30 +0200

    removed use of JELLY_BEAN_MR1 so that the lib compiles with targetSdk=16

--- library/src/com/manuelpeinado/fadingactionbar/FadingActionBarHelper.java
+++ library/src/com/manuelpeinado/fadingactionbar/FadingActionBarHelper.java
@@ -94,7 +94,7 @@ public class FadingActionBarHelper {
     }
 
     private void addDrawableCallback() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN) {
             mActionBarBackgroundDrawable.setCallback(mDrawableCallback);
         }
     }
